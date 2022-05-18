9e14d0742957a858cd53d7e1283d7d38a4f3ac1a
Matthew Bailey <mattbailey1861@gmail.com>
2022-05-18 10:32:48 +0100

    start-of-backend

---
+++ models/items.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+
+const itemsSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+})
+
+export default mongoose.model('Items', itemsSchema)
