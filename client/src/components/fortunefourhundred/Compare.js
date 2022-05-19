c234a411e79bfbcbd5d6c18e79863791ea042316
j-lgrant <justingrant373@gmail.com>
2022-05-19 18:33:18 +0100

    search bar tracking names

--- client/src/components/fortunefourhundred/Compare.js
+++ client/src/components/fortunefourhundred/Compare.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { useParams, Link } from 'react-router-dom'
-// import { v4 as uuidv4 } from 'uuid'
+
+
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -10,85 +10,112 @@ import Card from 'react-bootstrap/Card'
 const Compare = () => {
 
   const [billionaires, setBillionaires] = useState([])
-  const [filteredCharacters, setFilteredCharacters] = useState([])
+  const [errors, setErrors] = useState(false)
+  const [loading, setLoading] = useState(true)
+
+
   const [filters, setFilters] = useState({
-    names: 'All',
-    searchTerm: '',
+    names: '',
   })
+
 
 
   useEffect(() => {
     const getBillionaires = async () => {
       try {
         const { data } = await axios.get('https://forbes400.herokuapp.com/api/forbes400/')
-        console.log(data)
-        // console.log(data[1].person.name)
-        // console.log(data[0].finalWorth)
-        // console.log(data[0].bios)
         setBillionaires(data)
       } catch (error) {
-        console.log(error)
+        setErrors(true)
       }
+      setLoading(false)
     }
     getBillionaires()
   }, [])
 
 
-  // const handleChange = (e) => {
-  //   const newObj = {
-  //     ...filters,
-  //     [e.target.name]: e.target.value,
-  //   }
-  //   setFilters(newObj)
-  // }
+  // 
+  const handleSubmit = (e) => {
+    e.prevent.default()
+    console.log('sub')
+  }
 
-  // useEffect(() => {
-  //   if (billionaires.length) {
-  //     const billionaireList = []
-  //     billionaires.forEach(indiviual => billionaireList.includes(indiviual.name) ? '' : billionaireList.push(indiviual.name))
-  //     setBillionaires(billionaireList)
-  //     console.log(billionaireList) // names are located here. 
-  //   }
-  // }, [billionaires])
+  const handleChange = (e) => {
+    console.log(e.target.name, e.target.value)
+    setFilters({
+      ...filters,
+      [e.target.name]: e.target.value,
+    })
+
+  }
+
+
+
+
 
 
 
   /// Billionaire fortune 400 list; 
-
   return (
-    <>
-      <Container className='billionaire-list'>
-        <Row>
-          {billionaires.map(billionaire => {
-            const { uri, personName, squareImage, finalWorth, abouts } = billionaire
-            // console.log(uri)
-            return (
-              <Col key={uri} md='5' lg='4' className='character mb-4'>
-                <Card style={{ width: '18rem' }}>
-                  <Card.Img variant='top' src={squareImage} />
-                  <Card.Body className='bd-light'>
-                    <Card.Title className='text-center mb-1'>
-                      <h1>Name<span></span></h1>{personName}
-                      <h2><span>Networth</span></h2>${finalWorth}
-                      <h3><span>Description</span></h3><p>{abouts}</p></Card.Title>
-                  </Card.Body>
-                </Card>
-              </Col>
-            )
-          })}
-        </Row>
-      </Container>
-    </>
+    <main className='container'>
+      {loading ?
+        <p>Loading....</p>
+        :
+        errors ?
+          <p>Unable to get data, please try again!</p>
+          :
+          <>
+            <form onSubmit={handleSubmit}>
+              <input type='text' name='names' placeholder='Search...' id='names' value={filters.names} onChange={handleChange} />
+
+
+            </form>
+
+            <Container className='billionaire-list'>
+              <Row>
+                {billionaires.map(billionaire => {
+                  const { uri, personName, squareImage, finalWorth, abouts } = billionaire
+                  // console.log(uri)
+                  return (
+                    <Col key={uri} md='5' lg='4' className='character mb-4'>
+                      <Card style={{ width: '18rem' }}>
+                        <Card.Img variant='top' src={squareImage} />
+                        <Card.Body className='bd-light'>
+                          <Card.Title className='text-center mb-1'>
+                            <h1>Name<span></span></h1>{personName}
+                            <h2><span>Net worth</span></h2>${finalWorth}
+                            <h3><span>Description</span></h3><p>{abouts}</p></Card.Title>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  )
+                })}
+              </Row>
+            </Container>
+          </>
+      }
+    </main>
+
+
+
+
   )
-
-
-
-
-
-
-
-
-
 }
 
 export default Compare
+
+
+{/* <Container className='middle-col'>
+        <nav className="navbar navbar-light bg-light">
+          <a className="navbar-brand" href="#">Fortune400</a>
+          <a className="navbar-brand" href="#">Products</a>
+          <a className="navbar-brand" href="#">Misc</a>
+          <a className="navbar-brand" href="#">Users</a>
+          <a className="navbar-brand" href="#">Celebrities</a>
+        </nav> */}
+
+{/* Search Bar */ }
+{/* <input className='form-control form-control-sm' id='2' type='text'
+          name='searchTerm' placeholder='Search..' /> */}
+{/* 
+    </Container> */}
