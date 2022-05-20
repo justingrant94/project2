00d7bb7f60fcfa1a4d2ce6da81d71c95514a7670
j-lgrant <justingrant373@gmail.com>
2022-05-20 12:07:24 +0100

    friday

--- client/src/components/fortunefourhundred/Compare.js
+++ client/src/components/fortunefourhundred/Compare.js
@@ -10,95 +10,109 @@ import Card from 'react-bootstrap/Card'
 const Compare = () => {
 
   const [billionaires, setBillionaires] = useState([])
-  const [errors, setErrors] = useState(false)
-  const [loading, setLoading] = useState(true)
-
-
+  const [filteredBillionaires, setFilteredBillionaires] = useState([])
+  const [genders, setGenders] = useState([])
   const [filters, setFilters] = useState({
-    names: '',
+    gender: 'All',
+    searchTerm: '',
   })
-
-
 
   useEffect(() => {
     const getBillionaires = async () => {
       try {
         const { data } = await axios.get('https://forbes400.herokuapp.com/api/forbes400/')
+        // console.log(data)
+        // console.log(data[1].person.name)
+        // console.log(data[0].finalWorth)
+        // console.log(data[0].bios)
         setBillionaires(data)
       } catch (error) {
-        setErrors(true)
+        console.log(error)
       }
-      setLoading(false)
     }
     getBillionaires()
   }, [])
 
-
-  // 
-  const handleSubmit = (e) => {
-    e.prevent.default()
-    console.log('sub')
-  }
-
+  // This changes the gender of the the state
   const handleChange = (e) => {
-    console.log(e.target.name, e.target.value)
-    setFilters({
+    console.log(e.target.value)
+    const newObj = {
       ...filters,
       [e.target.name]: e.target.value,
-    })
-
+    }
+    setFilters(newObj)
   }
 
+  // //? useEffect to track the gender differences. 
+  useEffect(() => {
+    //checking there are genders to loop through in the first place
+    //on initial page load, the genders wukk empty so we dont need to create a list
+    if (billionaires.length) {
+      const genderList = []
+      billionaires.forEach(billionaire => {
+        if (!genderList.includes(billionaire.gender)) {
+          if (billionaire.gender) genderList.push(billionaire.gender)
+          else if (!genderList.includes('Undefined')) genderList.push('Undefined')
+        }
+      })
+      setGenders(genderList)
+    }
+  }, [billionaires])
 
 
+  // useEffect that filtered the billionaires and adds them into the filtered state.
+  useEffect(() => {
+    //only filters billionaires if there are people to filter.
+    if (billionaires.length) {
+      //generate search term 
+      const regexSearch = new RegExp(filters.searchTerm, 'i')
+      console.log(regexSearch)
+      //filter through billionaires and ass matching people to fikteredbillionaire state
+      const filtered = billionaires.filter(billionaire => {
+        return regexSearch.test(billionaire.name) && (billionaire.gender === filters.gender || filters.gender === 'All')
+      })
+      setFilteredBillionaires(filtered)
+    }
+  }, [filters, billionaires])
 
 
-
-
-  /// Billionaire fortune 400 list; 
   return (
-    <main className='container'>
-      {loading ?
-        <p>Loading....</p>
-        :
-        errors ?
-          <p>Unable to get data, please try again!</p>
-          :
-          <>
-            <form onSubmit={handleSubmit}>
-              <input type='text' name='names' placeholder='Search...' id='names' value={filters.names} onChange={handleChange} />
+    <>
+      <div className='container'>
+        <div className='filter-container'>
+          <select name='gender' value={filters.gender} onChange={handleChange}>
+            <option key='All' value="All">All</option>
+            {genders.map(gender => <option key={gender} value={gender}>{gender}</option>)}
+          </select>
+          {/* Search Field */}
+          <input type='text' name='searchTerm' placeholder='Search...' value={filters.searchTerm} onChange={handleChange} />
+        </div>
+      </div>
 
 
-            </form>
-
-            <Container className='billionaire-list'>
-              <Row>
-                {billionaires.map(billionaire => {
-                  const { uri, personName, squareImage, finalWorth, abouts } = billionaire
-                  // console.log(uri)
-                  return (
-                    <Col key={uri} md='5' lg='4' className='character mb-4'>
-                      <Card style={{ width: '18rem' }}>
-                        <Card.Img variant='top' src={squareImage} />
-                        <Card.Body className='bd-light'>
-                          <Card.Title className='text-center mb-1'>
-                            <h1>Name<span></span></h1>{personName}
-                            <h2><span>Net worth</span></h2>${finalWorth}
-                            <h3><span>Description</span></h3><p>{abouts}</p></Card.Title>
-                        </Card.Body>
-                      </Card>
-                    </Col>
-                  )
-                })}
-              </Row>
-            </Container>
-          </>
-      }
-    </main>
-
-
-
-
+      <Container className='billionaire-list'>
+        <Row>
+          {(filteredBillionaires.length ? filteredBillionaires : billionaires).map(billionaire => {
+            const { uri, personName, squareImage, finalWorth, abouts } = billionaire
+            // console.log(uri)
+            return (
+              ///the left column of the billionaires
+              <Col key={uri} md='5' lg='4' className='character mb-4'>
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant='top' src={squareImage} />
+                  <Card.Body className='bg-light'>
+                    <Card.Title className='text-center mb-1'>
+                      <h1>Name<span></span></h1>{personName}
+                      <h2><span>Net worth</span></h2>${finalWorth}
+                      <h3><span>Description</span></h3><p>{abouts}</p></Card.Title>
+                  </Card.Body>
+                </Card>
+              </Col>
+            )
+          })}
+        </Row>
+      </Container>
+    </>
   )
 }
 
