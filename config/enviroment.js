e28b745b940cc6d02c8db61f1f33fbdc9f917195
Matthew Bailey <mattbailey1861@gmail.com>
2022-05-24 11:46:38 +0100

    l

--- config/enviroment.js
+++ config/enviroment.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-export const MONGODB_CONNECTION_STRING = 'mongodb://127.0.0.1:27017/worthy'
-export const PORT = 4000
-=======
 export const MONGODB_CONNECTION_STRING = 'mongodb://127.0.0.1:27017/items'
 export const PORT = 5000
->>>>>>> 6914d4b5a81478f6a7c394e19878f926d5bcc9d3
 export const SECRET = '5u4(9;ukxV:BscA>'
